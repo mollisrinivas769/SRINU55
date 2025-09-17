2038e5d05040cdacb20942c9454dfe72edc6897d
mollisrinivas769 <mollisrinivas769@gmail.com>
2025-09-17 10:05:36 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student{
+String name;
+int rno;
+Student()
+{
+  name="Abc";
+  rno=148;
+}
+public static void main(String arg[])
+{
+//creating an object
+Student s1=new Student();
+System.out.println(s1.name);
+System.out.println(s1.rno);
+}
+}
+
